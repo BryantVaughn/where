cbb0fa9de1d0e235e159dae331b74eeaac430c77
BryantVaughn <bryant.f.vaughn@gmail.com>
2021-01-19 14:02:32 -0700

    Initialize class fields

---
+++ src/User.ts
@@ -0,0 +1,25 @@
+import faker from 'faker';
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = this.generateName();
+    this.location = this.generateCoordinates();
+  }
+
+  private generateName(): string {
+    return `${faker.name.firstName()} ${faker.name.lastName()}`;
+  }
+
+  private generateCoordinates(): { lat: number; lng: number } {
+    const lat = parseFloat(faker.address.latitude());
+    const lng = parseFloat(faker.address.longitude());
+    console.log({ lat, lng });
+    return { lat, lng };
+  }
+}
